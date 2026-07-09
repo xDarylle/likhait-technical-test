dafac260a88c912cdb27335657b8047af4cb0fbb
xDarylle <daryllejumalon16@gmail.com>
2026-07-10 00:42:29 +0800

    feat(backend): include category_id & category_emoji to expense

--- backend/app/controllers/api/expenses_controller.rb
+++ backend/app/controllers/api/expenses_controller.rb
@@ -52,7 +52,9 @@ class Api::ExpensesController < ApplicationController
       id: expense.id,
       description: expense.description,
       amount: expense.amount.to_f,
+      category_id: expense.category_id,
       category: expense.category.name,
+      category_emoji: expense.category.emoji,
       date: expense.date.to_s,
       created_at: expense.created_at,
       updated_at: expense.updated_at
